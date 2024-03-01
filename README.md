3a7ad162bb8614c98d35c25529a7578cd09823a0
zoeng <zoe.ng@utexas.edu>
2024-03-01 15:45:19 -0600

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# charlies-donuts-v2
+Charlie's Donuts
+
+This is a website for a donut shop in Victoria, TX. I designed the website on Figma, and I used a Gatsby starter template
+(https://github.com/gatsbyjs/gatsby-starter-hello-world) to develop the site.
---
+++ gatsby-config.js
@@ -0,0 +1,54 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Poppins`,
+            variants: [`400`, `700`]
+          }
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/assets/`,
+        name: `assets`
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Charlie's Donuts`,
+        short_name: `Charlie's Donuts`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `browser`,
+        icon: `src/assets/donut4.png`
+      },
+    },
+  ],
+}
+
+
