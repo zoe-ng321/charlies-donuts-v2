afcfa23a1691f8233303380a8901e76cd4be319c
zoeng <zoe.ng@utexas.edu>
2024-03-01 17:51:13 -0600

    update gitignore

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -1,5 +1,6 @@
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=default" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-about-js": () => import("./../../../src/pages/about.js?export=default" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-contact-js": () => import("./../../../src/pages/contact.js?export=default" /* webpackChunkName: "component---src-pages-contact-js" */),
   "component---src-pages-gallery-js": () => import("./../../../src/pages/gallery.js?export=default" /* webpackChunkName: "component---src-pages-gallery-js" */),
